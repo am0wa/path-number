46a4a0f942784edabe7d00d5d72d8a785a0f26aa
Andrii Murashko <Andrii.Murashko@playtech.com>
2020-04-24 17:36:15 +0300

    Updates

---
+++ precache-manifest.46dde7c78c05a2e79942110ba249bf1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e518fd0c68193c6366c12f84c16b8fc0",
+    "url": "/path-number/index.html"
+  },
+  {
+    "revision": "3938f3bd567c9ddcb7bb",
+    "url": "/path-number/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "5dde120256468683159a",
+    "url": "/path-number/static/css/main.f925dd06.chunk.css"
+  },
+  {
+    "revision": "3938f3bd567c9ddcb7bb",
+    "url": "/path-number/static/js/2.b20e7fd4.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/path-number/static/js/2.b20e7fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dde120256468683159a",
+    "url": "/path-number/static/js/main.6c8b113f.chunk.js"
+  },
+  {
+    "revision": "13802e232aa52cb0fa83",
+    "url": "/path-number/static/js/runtime-main.a2b90f66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/path-number/static/media/logo.5d5d9eef.svg"
+  }
+]);
